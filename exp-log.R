8ca6ea9c3ac2ead4428846c843b93a62d4a3f4a4
Leonardo (Leo) E. Manríquez, MS <60280278+leomanriquezm@users.noreply.github.com>
2023-07-12 00:17:36 -0400

    Update exp-log.R

--- exp-log.R
+++ exp-log.R
@@ -1,10 +1,11 @@
+#new logarithm definition
 my_ln<-function(x,u){
     #u: umbral
     y<-ifelse(x<u,u,x)
     return(log(y))
   }
   
-  #Definicipon de nuevo exp
+#new exponential definition
   my_exp<-function(x,u){
     #u: umbral
     y<-ifelse(x>=(-u) & x<u, x, ifelse(x<(-u),-u,u))
